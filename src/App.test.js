8fa172f1e26b4b3cdc7db0f37da5d273f95e16ec
Brad Mortensen <b.mort86@gmail.com>
2018-12-05 11:40:03 -0700

    Adds Enzyme and Associated Plumbing

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+
 
 
 describe('<App />', ()=>{
+  it('shallow renders without crashing', ()   => {
+    shallow(<App />); // notice this only renders App and will not render any child components
+  });
   it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
