31d67ef9357434c0a8d15058ffa5d0b2f9c0b911
Brad Mortensen <b.mort86@gmail.com>
2018-12-05 12:36:14 -0700

    Adds Additional Tests

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    isOn: false,
+  };
+
   render() {
+    const displayText = this.state.isOn ? 'On' : 'Off';
+
     return (
       <div className="App">
-        <h1 className='display'>Switch</h1>
+        <h1 className="display">{displayText}</h1>
+
+        <div>
+          <button onClick={this.toggle} className="toggle-btn">
+            Toggle
+          </button>
+        </div>
       </div>
     );
   }
+
+  toggle = () => {
+    this.setState(prevState => {
+      return { isOn: !prevState.isOn };
+    });
+  };
 }
 
 export default App;
